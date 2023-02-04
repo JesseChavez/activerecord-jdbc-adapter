a040b9771b3f4e4f6f6ffa33af4178e54ec10992
Jesse Chavez <jesse.chavez.r@gmail.com>
2023-02-04 13:33:06 +1100

    Fix loading mssql driver to be able run tests

--- test/db/mssql_config.rb
+++ test/db/mssql_config.rb
@@ -21,18 +21,25 @@ end
 if ENV['DRIVER'] =~ /jTDS/i
   # change adapter for jTDS
   MSSQL_CONFIG[:adapter] = 'mssql'
-else
-  # Using MS official  SQL JDBC driver
-  require 'jdbc/sqlserver'
-  begin
-    silence_warnings { Java::JavaClass.for_name(Jdbc::SQLServer.driver_name) }
-  rescue NameError
-    begin
-      Jdbc::SQLServer.load_driver
-    rescue LoadError => e
-      warn "If you want to use a specific version please setup the mssql driver to run the MS-SQL tests!\n#{e}"
-      require 'jdbc/mssql'
-      warn "using the jdbc-mssql gem to run test."
-    end
-  end
+  return
+end
+
+# Using MS official  SQL JDBC driver
+require 'jdbc/sqlserver'
+
+# NOTE: the below rescue does not work anymore.
+# begin
+#   silence_warnings { Java::JavaClass.for_name(Jdbc::SQLServer.driver_name) }
+#   return
+# rescue NameError => e
+#   warn "unable to load java class with name: #{e}"
+# end
+
+begin
+  Jdbc::SQLServer.load_driver
+rescue LoadError => e
+  warn "If you want to use a specific version please setup the mssql driver to run the MS-SQL tests!\n#{e}"
+  warn "Place the jar in the folder 'test/jars/' and update the jar name in test/jdbc/sqlserver.rb file"
+  require 'jdbc/mssql'
+  warn 'using the jdbc-mssql gem to run test.'
 end
