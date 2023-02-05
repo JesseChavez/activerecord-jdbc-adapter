ec7094574c29bad0a8f53c26f374e48ff16f143f
Jesse Chavez <jesse.chavez.r@gmail.com>
2023-02-05 18:25:35 +1100

    Fix sqlserver specific types definitions, column options argument changed to double splat form

--- lib/arjdbc/mssql/schema_definitions.rb
+++ lib/arjdbc/mssql/schema_definitions.rb
@@ -14,61 +14,61 @@ module ActiveRecord
 
         # datetime with seconds always zero (:00) and without fractional seconds
         def smalldatetime(*args, **options)
-          args.each { |name| column(name, :smalldatetime, options) }
+          args.each { |name| column(name, :smalldatetime, **options) }
         end
 
         # this is the old sql server datetime type, the precision is as follow
         # xx1, xx3, and xx7
         def datetime_basic(*args, **options)
-          args.each { |name| column(name, :datetime_basic, options) }
+          args.each { |name| column(name, :datetime_basic, **options) }
         end
 
         def real(*args, **options)
-          args.each { |name| column(name, :real, options) }
+          args.each { |name| column(name, :real, **options) }
         end
 
         def money(*args, **options)
-          args.each { |name| column(name, :money, options) }
+          args.each { |name| column(name, :money, **options) }
         end
 
         def smallmoney(*args, **options)
-          args.each { |name| column(name, :smallmoney, options) }
+          args.each { |name| column(name, :smallmoney, **options) }
         end
 
         def char(*args, **options)
-          args.each { |name| column(name, :char, options) }
+          args.each { |name| column(name, :char, **options) }
         end
 
         def varchar(*args, **options)
-          args.each { |name| column(name, :varchar, options) }
+          args.each { |name| column(name, :varchar, **options) }
         end
 
         def varchar_max(*args, **options)
-          args.each { |name| column(name, :varchar_max, options) }
+          args.each { |name| column(name, :varchar_max, **options) }
         end
 
         def text_basic(*args, **options)
-          args.each { |name| column(name, :text_basic, options) }
+          args.each { |name| column(name, :text_basic, **options) }
         end
 
         def nchar(*args, **options)
-          args.each { |name| column(name, :nchar, options) }
+          args.each { |name| column(name, :nchar, **options) }
         end
 
         def ntext(*args, **options)
-          args.each { |name| column(name, :ntext, options) }
+          args.each { |name| column(name, :ntext, **options) }
         end
 
         def binary_basic(*args, **options)
-          args.each { |name| column(name, :binary_basic, options) }
+          args.each { |name| column(name, :binary_basic, **options) }
         end
 
         def varbinary(*args, **options)
-          args.each { |name| column(name, :varbinary, options) }
+          args.each { |name| column(name, :varbinary, **options) }
         end
 
         def uuid(*args, **options)
-          args.each { |name| column(name, :uniqueidentifier, options) }
+          args.each { |name| column(name, :uniqueidentifier, **options) }
         end
       end
 
