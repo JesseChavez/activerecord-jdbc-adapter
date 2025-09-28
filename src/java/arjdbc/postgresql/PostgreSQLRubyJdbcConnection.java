8a8273904debb67beb4aa7a662206ebdf8929c56
Jesse Chavez <jesse.chavez.r@gmail.com>
2025-09-28 12:50:37 +0900

    Postgres, pass type casted binds for select queries

--- src/java/arjdbc/postgresql/PostgreSQLRubyJdbcConnection.java
+++ src/java/arjdbc/postgresql/PostgreSQLRubyJdbcConnection.java
@@ -691,7 +691,7 @@ public class PostgreSQLRubyJdbcConnection extends arjdbc.jdbc.RubyJdbcConnection
             }
         }
 
-        super.setStringParameter(context, connection, statement, index, value, attribute, type);
+        statement.setObject(index, value.asString().toString(), Types.OTHER);
     }
 
 
